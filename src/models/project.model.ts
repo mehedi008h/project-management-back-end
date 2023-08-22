3148a8935b0f5d1612e13f9e0ab2e60e2a58d6ad
Mehedi Hasan <mehedi08h@gmail.com>
2023-08-22 16:19:33 +0600

    create project model

---
+++ src/models/project.model.ts
@@ -0,0 +1,45 @@
+import { Schema, model } from "mongoose";
+import { IProject } from "../domain/project";
+
+const ProjectSchema: Schema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Please enter your project title"],
+        },
+        projectIdentifier: {
+            type: Number,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: [true, "Please enter your project description"],
+        },
+        startDate: {
+            type: Date,
+            required: [true, "Please enter your project start date"],
+        },
+        endDate: {
+            type: Date,
+            required: [true, "Please enter your project end date"],
+        },
+        photo: {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            },
+        },
+        tags: [],
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { timestamps: true }
+);
+
+export default model<IProject>("Project", ProjectSchema);
